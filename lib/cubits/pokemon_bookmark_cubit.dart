45903e942ee2a249194e0604c489e4fee107618f
danielamaric <daniela.maric98@icloud.com>
2022-12-30 17:23:18 +0100

    hydrated bloc for state persistance

--- lib/cubits/pokemon_bookmark_cubit.dart
+++ lib/cubits/pokemon_bookmark_cubit.dart
@@ -1,6 +1,6 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 
-class PokemonBookmarkCubit extends Cubit<List<String>> {
+class PokemonBookmarkCubit extends HydratedCubit<List<String>> {
   PokemonBookmarkCubit() : super([]);
 
   void addPokemonName(String value) {
@@ -14,4 +14,11 @@ class PokemonBookmarkCubit extends Cubit<List<String>> {
     newList.remove(value);
     emit(newList);
   }
+
+  @override
+  List<String> fromJson(Map<String, dynamic> json) =>
+      json['value'] as List<String>;
+
+  @override
+  Map<String, List<String>> toJson(List<String> state) => {'value': state};
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:mini_pokedex/cubits/pokemon_bookmark_cubit.dart';
 import 'package:mini_pokedex/main_menu_screen/main_menu_screen.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final directory = await getApplicationDocumentsDirectory();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: directory,
+  );
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
